ed27658a1354979395d8b77d581c46b09492594d
kensuke-m <miyasita@cs.kyoto-wu.ac.jp>
2017-10-23 10:24:07 +0000

    use dotenv with unicorn

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,18 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require "dotenv"
+
+dotenv_root = File.expand_path("../../", __FILE__)
+dotenv_env = ENV.fetch("RAILS_ENV", ENV.fetch("RACK_ENV", "development"))
+dotenv_files = ["#{dotenv_root}/.env.#{dotenv_env}", "#{dotenv_root}/.env"]
+
+if ENV.include? "DOTENV_LOADED"
+  ENV["DOTENV_LOADED"].split(":").each do |key|
+    ENV.delete(key)
+  end
+end
+
+dotenv_loaded = Dotenv.load(*dotenv_files)
+
+ENV["DOTENV_LOADED"] = dotenv_loaded.keys.join(":")
