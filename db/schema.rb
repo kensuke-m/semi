e1947b595b7cde087b312d14132289ca5532ff73
kensuke-m <miyasita@cs.kyoto-wu.ac.jp>
2017-11-01 17:40:19 +0000

    course table added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028205953) do
+ActiveRecord::Schema.define(version: 20171030065017) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
